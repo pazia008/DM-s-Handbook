722487e26db1829f331133484eb46ac55f90e017
Pazia Ramirez <pazia008@gmail.com>
2021-04-29 15:57:39 -0500

    made a userprofile model

---
+++ DM-Handbook/DM-Handbook/Models/UserProfiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DM_Handbook.Models
+{
+    public class UserProfiles
+    {
+        public class UserProfile
+        {
+            public int Id { get; set; }
+
+            [Required]
+            [MaxLength(50)]
+            public string Userame { get; set; }
+
+            [Required]
+            [MaxLength(50)]
+            public string Email { get; set; }
+
+            public string FirebaseId { get; set; }
+
+        }
+    }
+}
