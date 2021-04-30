fc82dd183f67c1b1a6f72100283ad6e3ee70f420
Pazia Ramirez <pazia008@gmail.com>
2021-04-30 10:57:05 -0500

    Got the name of my app and my login link in the nav bar to show up on the dom

---
+++ DM-Handbook/DM-Handbook/client/src/components/Header.js
@@ -0,0 +1,117 @@
+import React, { useState, useContext } from "react";
+import { NavLink as RRNavLink } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from "reactstrap";
+import { UserProfilesContext } from "../providers/UserProfilesProvider";
+
+export default function Header() {
+    const { isLoggedIn, logout } = useContext(UserProfilesContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand tag={RRNavLink} to="/">
+                    DM's Handbook
+        </NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+
+                        {/* When isLoggedIn === true, we will render the Home link */}
+                        {isLoggedIn && (
+                            <>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/">
+                                        Home
+                </NavLink>
+                                </NavItem>
+                            </>
+                        )}
+                    </Nav>
+
+                    <Nav className="mr-auto" navbar>
+                        {/* When isLoggedIn === true, we will render the Posts link */}
+                        {isLoggedIn && (
+                            <NavItem>
+                                <NavLink tag={RRNavLink} to="/post">
+                                    Posts
+                </NavLink>
+                            </NavItem>
+                        )}
+                    </Nav>
+
+                    <Nav className="mr-auto" navbar>
+                        {/* When isLoggedIn === true, we will render the My Posts link */}
+                        {isLoggedIn && (
+                            <NavItem>
+                                <NavLink tag={RRNavLink} to="/my-posts">
+                                    My Posts
+                </NavLink>
+                            </NavItem>
+                        )}
+                    </Nav>
+
+                    <Nav className="mr-auto" navbar>
+                        {isLoggedIn && (
+                            <NavItem>
+                                <NavLink tag={RRNavLink} to="/tag">
+                                    Tags
+                </NavLink>
+                            </NavItem>
+                        )}
+                    </Nav>
+                    <Nav className="mr-auto" navbar>
+                        {isLoggedIn && (
+                            <NavItem>
+                                <NavLink tag={RRNavLink} to="/category">
+                                    Categories
+                </NavLink>
+                            </NavItem>
+                        )}
+                    </Nav>
+                    <Nav navbar>
+                        {isLoggedIn && (
+                            <>
+                                <NavItem>
+                                    <a
+                                        aria-current="page"
+                                        className="nav-link"
+                                        style={{ cursor: "pointer" }}
+                                        onClick={logout}
+                                    >
+                                        Logout
+                  </a>
+                                </NavItem>
+
+                            </>
+                        )}
+
+                        {!isLoggedIn && (
+                            <>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/login">
+                                        Login
+                  </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/register">
+                                        Register
+                  </NavLink>
+                                </NavItem>
+                            </>
+                        )}
+                    </Nav>
+                </Collapse>
+            </Navbar >
+        </div >
+    );
+}
--- DM-Handbook/DM-Handbook/client/src/index.js
+++ DM-Handbook/DM-Handbook/client/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import firebase from "firebase/app";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import reportWebVitals from './reportWebVitals';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -20,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister();
+reportWebVitals();
