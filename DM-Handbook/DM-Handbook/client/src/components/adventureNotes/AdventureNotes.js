c326cadada7e59f05ce32e11b87fe586d55c0feb
Pazia Ramirez <pazia008@gmail.com>
2021-05-06 22:46:33 -0500

    users can edit and delete adventure notes

--- DM-Handbook/DM-Handbook/client/src/components/adventureNotes/AdventureNotes.js
+++ DM-Handbook/DM-Handbook/client/src/components/adventureNotes/AdventureNotes.js
@@ -2,9 +2,17 @@ import React from "react";
 import { Card, CardHeader } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
+import { useContext } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import { AdventureNotesContext } from "../../providers/AdventureNotesProvider";
 
 const AdventureNote = ({ adventureNote }) => {
+
+    const { deleteAdventureNote } = useContext(AdventureNotesContext);
+
+    const noteDelete = () => {
+        deleteAdventureNote(adventureNote);
+    }
 
     let date = new Date(adventureNote.dateCreated);
     console.log(adventureNote)
@@ -28,6 +36,17 @@ const AdventureNote = ({ adventureNote }) => {
             }} className="edit-button">
                 Edit
                     </Button>
+
+            <Button variant="secondary" onClick={() => {
+                const confirmBox = window.confirm(
+                    "Do you really want to delete this Adventure?"
+                )
+                if (confirmBox === true) {
+                    noteDelete(deleteAdventureNote)
+                }
+            }} className="delete-button">
+                Delete
+                </Button>
         </Card>
     );
 };
