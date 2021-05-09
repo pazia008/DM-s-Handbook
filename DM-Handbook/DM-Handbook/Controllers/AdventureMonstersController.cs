e0978d000655368dab63439a159ffe3123b51001
Pazia Ramirez <pazia008@gmail.com>
2021-05-09 17:34:00 -0500

    attempting to add monsters to adventure notes through swagger

---
+++ DM-Handbook/DM-Handbook/Controllers/AdventureMonstersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DM_Handbook.Models;
+using DM_Handbook.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static DM_Handbook.Models.UserProfiles;
+
+namespace DM_Handbook.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdventureMonstersController : ControllerBase
+    {
+
+
+        private readonly IAdventureMonstersRepository _adventureMonstersRepository;
+        private readonly IUserProfilesRepository _userProfilesRepository;
+
+        public AdventureMonstersController(IAdventureMonstersRepository adventureMonstersRepository, IUserProfilesRepository userProfilesRepository)
+        {
+            _adventureMonstersRepository = adventureMonstersRepository;
+            _userProfilesRepository = userProfilesRepository;
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(AdventureMonsters adventureMonsters)
+        {
+            var currentUserProfile = GetCurrentUser();
+            _adventureMonstersRepository.AddAdventureMonster(adventureMonsters);
+
+            return Ok(adventureMonsters);
+        }
+
+
+        private UserProfile GetCurrentUser()
+        {
+            var firebaseId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfilesRepository.GetByFirebaseUserId(firebaseId);
+        }
+
+    }
+}
---
+++ DM-Handbook/DM-Handbook/Repositories/IAdventureMonstersRepository.cs
@@ -0,0 +1,9 @@
+﻿using DM_Handbook.Models;
+
+namespace DM_Handbook.Repositories
+{
+    public interface IAdventureMonstersRepository
+    {
+        void AddAdventureMonster(AdventureMonsters adventureMonsters);
+    }
+}
