c18fe82a5dd06f03c030bce84501a1cef16cf452
Pazia Ramirez <pazia008@gmail.com>
2021-05-13 10:49:58 -0500

    users can virtually roll dice

--- DM-Handbook/DM-Handbook/client/src/components/Hello.js
+++ DM-Handbook/DM-Handbook/client/src/components/Hello.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Button } from "reactstrap";
 import dFour from "./pleswork.png";
 import dSix from "./colored6.png";
@@ -11,9 +11,43 @@ import "./Hello.css";
 export default function Hello() {
     let currentUser = JSON.parse(sessionStorage.getItem("userProfile"));
 
+    const [number, setNumber] = useState()
+
+    const d4 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 4) + 1)
+
+    }
+    const d6 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 6) + 1)
+
+    }
+    const d8 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 8) + 1)
+
+    }
+    const d10 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 10) + 1)
+
+    }
+    const d12 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 12) + 1)
+
+    }
+    const d20 = () => {
+        console.log("hello")
+        setNumber(Math.floor(Math.random() * 20) + 1)
+
+    }
+
 
     return (
         <>
+            <div className="numberFour">{number}</div>
             <div style={{
                 position: "fixed",
                 left: 0,
@@ -22,19 +56,49 @@ export default function Hello() {
                 marginTop: "-0.5rem",
                 textAlign: "center",
             }}>
-                <div>May the dice roll forever in your favor!</div>
+
+
+                <div className="text">May the dice roll forever in your favor!</div>
 
                 <div className="flexRow">
-                    <img src={dFour} className="dFour" />
-                    <img src={dSix} className="dSix" />
-                    <img src={dEight} className="dEight" />
-                    <img src={dTen} className="dTen" />
-                    <img src={dTwelve} className="dTwelve" />
-                    <img src={dTwenty} className="dTwenty" />
+                    <div className="flexColumn">
+                        <img src={dFour} className="dFour" />
+                        <div className="flexButtons">
+                            <Button onClick={d4}>Roll a d4</Button>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <img src={dSix} className="dSix" />
+                        <div className="flexButtons">
+                            <Button onClick={d6}>Roll a d6</Button>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <img src={dEight} className="dEight" />
+                        <div className="flexButtons">
+                            <Button onClick={d8}>Roll a d8</Button>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <img src={dTen} className="dTen" />
+                        <div className="flexButtons">
+                            <Button onClick={d10}>Roll a d10</Button>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <img src={dTwelve} className="dTwelve" />
+                        <div className="flexButtons">
+                            <Button onClick={d12}>Roll a d12</Button>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <img src={dTwenty} className="dTwenty" />
+                        <div className="flexButtons">
+                            <Button onClick={d20}>Roll a d20</Button>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
 
         </>
 
